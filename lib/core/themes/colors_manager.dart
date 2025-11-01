0d23bc33adaa7033b35bb1dffc7e141e9c448e7f
omniamohamed <omnia.mohamed.dev@gmail.com>
2025-11-01 05:34:21 +0200

    -add native splash screen package&flutter svg&screenutil -add flutter navtive splash.yaml -add onbourding screen - -

---
+++ lib/core/themes/colors_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color primaryColor= Color(0xff247CFF);
+  static const Color primaryDarkColor=Color(0xff242424);
+  static const Color textFormFeildBackgroundColor=Color(0xffEDEDED);
+  static const Color primaryGreyColor=Color(0xff757575);
+  static const Color homeAppBarTextColor=Color(0xff616161);
+  static const Color seeMoreColor=Color(0xff247CFF);
+  static const Color seeMoreListBackgroundColor=Color(0xffF4F8FF);
+}
---
+++ lib/core/themes/font_styles_manager.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FontStylesManager {
+ static  TextStyle font32Bold = TextStyle(
+  fontSize: 32.sp,
+  fontWeight: FontWeight.bold,
+ );
+
+ static  TextStyle font12Regular = TextStyle(
+  fontSize: 12.sp,
+  fontWeight: FontWeight.normal,
+ );
+ static  TextStyle font11Regular = TextStyle(
+  fontSize: 11.sp,
+  fontWeight: FontWeight.normal,
+ );
+
+ static  TextStyle font24Regular = TextStyle(
+  fontSize: 24.sp,
+  fontWeight: FontWeight.normal,
+ );
+
+ static  TextStyle font16SemiBold = TextStyle(
+  fontSize: 16.sp,
+  fontWeight: FontWeight.w600,
+ );
+
+ static  TextStyle font24Bold = TextStyle(
+  fontSize: 24.sp,
+  fontWeight: FontWeight.bold,
+ );
+
+ static  TextStyle font14Regular = TextStyle(
+  fontSize: 14.sp,
+  fontWeight: FontWeight.normal,
+ );
+
+ static  TextStyle font14Medium = TextStyle(
+  fontSize: 14.sp,
+  fontWeight: FontWeight.w500,
+ );
+
+ static  TextStyle font18Medium = TextStyle(
+  fontSize: 18.sp,
+  fontWeight: FontWeight.w500,
+ );
+}
+
