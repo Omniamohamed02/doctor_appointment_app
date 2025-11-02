1e679f6fbcda89a61dbada62855604922bb1674a
omniamohamed <omnia.mohamed.dev@gmail.com>
2025-11-02 06:17:09 +0200

    add ScreenUtil.ensureScreenSize();

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:doctor_appointment_app/doc_doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+import 'core/themes/colors_manager.dart';
+
+void main() async {
+    await ScreenUtil.ensureScreenSize();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: ColorsManager.primaryBlueColor,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
   runApp(const DocDocApp());
 }
 
